5e6f6a3efe4cf1d7e6d0c6d759a1f28dc4864c39
Eduardo Peñarreta <espenarreta@utpl.edu.ec>
2024-10-24 12:28:39 -0500

    Ejercicio 8

---
+++ Ejercicio08/src/ejercicio08/Ejercicio08.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio08;
+
+/**
+ *
+ * @author utpl
+ */
+public class Ejercicio08 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        // Asignamos variables
+        
+        int a = 10;
+        int b = 9;
+        int c = 20;
+        int d = 500;
+        int e = 2;
+        int f = 3;
+        int g = 200;
+        int h = 50;
+        int i = 51;
+        
+        // Resolvemos el ejercicio
+        
+        System.out.println((a*b)+c-(d/e)+Math.pow(f,e)>=g || h >= h - h + i);
+    }
+    
+}
