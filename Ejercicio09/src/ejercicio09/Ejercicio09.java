e5282316e05455d210ebe971dcc295a5167fb7a3
Eduardo Peñarreta <espenarreta@utpl.edu.ec>
2024-10-24 12:31:54 -0500

    Ejercicio 9

---
+++ Ejercicio09/src/ejercicio09/Ejercicio09.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio09;
+
+/**
+ *
+ * @author utpl
+ */
+public class Ejercicio09 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        // Asignamos Valores
+        
+        int a = 81;
+        int b = 25;
+        int c = 10;
+        int d = 50;
+        
+        //Resolvemos ejercicio
+        
+        System.out.println(Math.sqrt(a) + Math.sqrt(b) * c >= d);
+    }
+    
+}
