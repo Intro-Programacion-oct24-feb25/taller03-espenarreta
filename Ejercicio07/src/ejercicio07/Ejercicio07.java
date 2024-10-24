761ce7a1b48d4cd49c62ef83d81cc56283735195
Eduardo Peñarreta <espenarreta@utpl.edu.ec>
2024-10-24 12:19:23 -0500

    Ejercicio 7

---
+++ Ejercicio07/src/ejercicio07/Ejercicio07.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio07;
+
+/**
+ *
+ * @author utpl
+ */
+public class Ejercicio07 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        // Asignamos variables
+        
+        int a = 81;
+        int b = 9;
+        int c = 3;
+        int d = 10;
+        int e = 1;
+        int f = 100;
+        int g = 25;
+        
+        System.out.println(((Math.sqrt(a)+b)/c==b) || (d>e) && ((f/g)>= Math.sqrt(f)));
+    }
+    
+}
