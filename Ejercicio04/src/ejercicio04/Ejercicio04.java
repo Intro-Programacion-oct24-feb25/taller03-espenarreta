a49a5b2c8c7bbbebd1864a98297fe01726799fde
Eduardo Peñarreta <espenarreta@uptl.edu.ec>
2024-10-24 11:50:56 -0500

    Ejercicio 4

---
+++ Ejercicio04/src/ejercicio04/Ejercicio04.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio04;
+
+/**
+ *
+ * @author utpl
+ */
+public class Ejercicio04 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        // Asignamos variables
+        
+        int a = 20;
+        int b = 1;
+        int c = 2;
+        int d = 3;
+        
+        // Resolvemos el eercicio
+        
+        System.out.println(a/(b+d)+c>=d);
+        
+    }
+    
+}
