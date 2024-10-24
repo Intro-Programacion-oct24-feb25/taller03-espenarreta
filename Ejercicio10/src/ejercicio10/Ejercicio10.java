888f2e8d071bf4b656faa63e5b571cfb4e8db93b
Eduardo Peñarreta <espenarreta@utpl.edu.ec>
2024-10-24 12:39:15 -0500

    Ejercicio 10

---
+++ Ejercicio10/src/ejercicio10/Ejercicio10.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio10;
+
+/**
+ *
+ * @author utpl
+ */
+public class Ejercicio10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        // Asignamos variables
+        
+        int a = 25;
+        int b = 10;
+        int c = 100;
+        int d = 5;
+        int e = 2;
+        boolean f = true;
+        boolean g = false;
+        
+        // Resolvemos
+        
+        System.out.println(Math.sqrt(a) * b >= c && (f) || (g) || (b/d >= e));
+    }
+    
+}
