f51c776e727f4e68a339c084c8ae63a3e5809960
Eduardo Peñarreta <espenarreta@uptl.edu.ec>
2024-10-24 11:35:01 -0500

    Ejercicio 3

--- Ejercicio03/src/ejercicio03/Ejercicio03.java
+++ Ejercicio03/src/ejercicio03/Ejercicio03.java
@@ -17,6 +17,18 @@ public class Ejercicio03 {
     public static void main(String[] args) {
         // TODO code application logic here
         
+        // Asignamos variables a todos los números
+        
+        int a = 10;
+        int b = 2;
+        int c = 16;
+        int d = 20;
+        int e = 1;
+        int f = 11;
+        
+        // Resilvemos el Ejercicio
+        
+        System.out.println(Math.pow(a,b)+(Math.sqrt(c))+(d/(e+f)));
     }
     
 }
